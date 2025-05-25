172bdc2661808c3a96347381b65c716fa70a6015
3lsy <elsychavez@protonmail.com>
2025-05-25 16:20:14 +0200

    corrected weechat script

--- weechat.sh
+++ weechat.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Usage: ./weechat.sh <nickname> <port> <password>
+# Usage: ./weechat.sh <nickname> <host> <port> <password>
 
 NICKNAME="$1"
-PORT="$2"
-PASSWORD="$3"
+HOST="$2"
+PORT="$3"
+PASSWORD="$4"
 
 # Basic validation
-if [[ -z "$NICKNAME" || -z "$PORT" || -z "$PASSWORD" ]]; then
-  echo "Usage: $0 <nickname> <port> <password>"
+if [[ -z "$NICKNAME" || -z "$HOST" || -z "$PORT" || -z "$PASSWORD" ]]; then
+  echo "Usage: $0 <nickname> <host> <port> <password>"
   exit 1
 fi
 
 CUID=$(head /dev/urandom | tr -dc 'a-f0-9' | head -c 6)
 
 docker run -it --rm --name "weechat-${NICKNAME}-${CUID}" --network host weechat/weechat:3.8 bash -c "
-  weechat -r '/server add myserver localhost/$PORT -notls' \
+  weechat -r '/server add myserver ${HOST}/${PORT} -notls' \
           -r '/set irc.server.myserver.nicks $NICKNAME' \
           -r '/set irc.server.myserver.username $NICKNAME' \
           -r '/set irc.server.myserver.realname Dockerized-WeeChat' \
@@ -24,5 +25,4 @@ docker run -it --rm --name "weechat-${NICKNAME}-${CUID}" --network host weechat/
           -r '/connect myserver'
 "
 
-echo "Weechat launched with nickname $NICKNAME, connecting to localhost:$PORT with password $PASSWORD"
-
+echo "Weechat launched with nickname $NICKNAME, connecting to $HOST:$PORT with password $PASSWORD"
